a298fd35cb2afb046cb8c1fc51d36d1057586d20
dev <dev@macs-Mac-mini.local>
2020-10-21 17:35:43 -0700

    commit updated etherxp NFT contract

--- solidity/contracts/ethxp.sol
+++ solidity/contracts/ethxp.sol
@@ -1,31 +1,15 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.6.2;
-
 /*
-Solidity base contracts imports are commented out --- all required solidity files are compiled into ./solidity/theetherxp_flattened.sol
-*/
-//import "../build/contracts/ERC721.sol";
-//import "../build/contracts/Counters.sol";
-//import "../build/contracts/ERC165.sol";
-//import "../build/contracts/IERC721Enumerable.sol";
-
-//import "/usr/local/lib/node_modules/openzeppelin-solidity/contracts/token/ERC721/ERC721.sol";
-//import "/usr/local/lib/node_modules/openzeppelin-solidity/contracts/utils/Counters.sol";
-//import "/usr/local/lib/node_modules/openzeppelin-solidity/contracts/introspection/ERC165.sol";
-//import "/usr/local/lib/node_modules/openzeppelin-solidity/contracts/token/ERC721/IERC721Enumerable.sol";
+This file is for documentation of the etherxp NFT contract - use ../etherxp-flattened.sol for compilation */
 //import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
 //import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/introspection/ERC165.sol";
 //import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
-
-
-
 
 contract theetherxp is ERC721 {
 
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIds;
-    constructor() public ERC721("TheEtherxp", "EXP") {}
+    constructor() public ERC721("TheEtherXP", "EXP") {}
     
     // global state variables
     mapping(uint256 => uint256) public AwardIdtoValue;
@@ -38,9 +22,11 @@ contract theetherxp is ERC721 {
              uint256 AwardId = _tokenIds.current();
             _mint(ethwallet, AwardId);
             _setTokenURI(AwardId, tokenURI);
+            // set Value
            AwardIdtoValue[AwardId] = Value;
         return AwardId;
     }
+    
 
     function getAwardValue(uint256 AwardId) public view returns(uint256) {
         return AwardIdtoValue[AwardId];
